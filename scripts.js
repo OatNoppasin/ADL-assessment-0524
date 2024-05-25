c1af62965b446ef30359e825fdc0f72b268bc5db
OatNoppasin <oat1100@gmail.com>
2024-05-25 11:50:03 +0700

    Fix V6

--- scripts.js
+++ scripts.js
@@ -74,7 +74,7 @@ document.getElementById('additionalForm').addEventListener('submit', (event) => 
 });
 
 function sendToGoogleSheets(data) {
-    fetch('https://script.google.com/macros/s/AKfycbw4dRDg3PHJgMnJocxuFjTLCsp-Y9ZUYrXEq0d3-yHXTQR_pRogOMN1_sxRsNw32SlT8Q/exec', {
+    fetch(API_URL, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
